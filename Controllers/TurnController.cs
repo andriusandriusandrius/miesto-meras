291a20985b89ee4d42f05662c73ddedf2117d6bb
andriusandriusandrius <and.zurba@gmail.com>
2025-11-13 15:21:32 +0200

    add turns and input

---
+++ Controllers/TurnController.cs
@@ -0,0 +1,35 @@
+using miesto_meras.Models;
+
+namespace miesto_meras.Controllers
+{
+    public class TurnController
+    {
+        private int TurnNumber{get;set;} = 1;
+
+        private readonly City _city;
+
+        public TurnController(City city)
+        {
+            _city = city;
+        }
+        private void PlayerInput()
+        {
+            Console.WriteLine("Your Turn:");
+            Console.ReadLine();
+        }
+        public void RunTurns(int maxTurns)
+        {
+            Console.WriteLine("===== ZAIDIMAS 'MIESTO MERAS' PRASIDEJO =====");
+            Console.WriteLine("");
+            while  (maxTurns >= TurnNumber)
+            {
+
+                Console.WriteLine($"Turn {TurnNumber}:");
+                _city.Display();
+                PlayerInput();
+                TurnNumber++;
+            }
+            Console.WriteLine("===== ZAIDIMAS 'MIESTO MERAS' PASIBAIGĖ =====");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using miesto_meras.Controllers;
+using miesto_meras.Models;
+
+City city = new("Siauliai", 40, 80, 10);
+
+TurnController turnController = new(city);
+
+turnController.RunTurns(10);
